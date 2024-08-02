0d58a747877c70c1688578b393100ce46863c6c9
LFzinn <luizferreiracod3@gmail.com>
2024-08-02 11:32:57 -0300

    feat: added angular defer for best performance

--- src/app/pages/home/properties/properties.component.ts
+++ src/app/pages/home/properties/properties.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 
 import { HousesListComponent } from '../../../shared/components/houses-list/houses-list.component';
 import { SectionTitleComponent } from '../../../shared/components/section-title/section-title.component';
+import { SkeletonComponent } from "../../../shared/utils/skeleton/skeleton.component";
 
 @Component({
   selector: 'app-properties',
   standalone: true,
-  imports: [HousesListComponent, SectionTitleComponent, CommonModule],
+  imports: [HousesListComponent, SectionTitleComponent, CommonModule, SkeletonComponent],
   templateUrl: './properties.component.html',
   styleUrl: './properties.component.css'
 })
