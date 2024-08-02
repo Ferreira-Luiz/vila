1e08e73991cb5e49653bff9b37ac2eae3de5dc8d
LFzinn <luizferreiracod3@gmail.com>
2024-08-02 14:32:35 -0300

    config: security configurations adjusted

---
+++ mynode.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+
+const envFile = `export const environment = {
+    apiKey: '${process.env.apiKey}',
+    authDomain: '${process.env.OTHER_authDomain}',
+    projectId: '${process.env.projectId}',
+    storageBucket: '${process.env.storageBucket}',
+    messagingSenderId: '${process.env.messagingSenderId}',
+    appId : '${process.env.appId }',
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+    }
+});
